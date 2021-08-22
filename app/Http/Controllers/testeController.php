495a11643e75adba6665586a80d5559354d4871a
Fernando Lira <it.fernandolira@gmail.com>
2021-08-22 12:04:48 +0100

    Enviar parametros do Controller para a View

--- app/Http/Controllers/testeController.php
+++ app/Http/Controllers/testeController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 class testeController extends Controller
 {
     public function teste(int $p1,int $p2) {
-        echo "A soma de $p1 + $p2 é: ".($p1+$p2);
+        //echo "A soma de $p1 + $p2 é: ".($p1+$p2);
+        
+        //Envio de parametros por array associativo
+        //return view('site.teste',['p1'=>$p1,'p2'=>$p2]);
+
+        //Envio de parametros pela função compact - Mais usado
+        return view('site.teste',compact('p1','p2'));
+
+        //Envio de parametros por With()
+        //return view('site.teste')->with('p1',$p1)->with('p2',$p2);
     }
 }
