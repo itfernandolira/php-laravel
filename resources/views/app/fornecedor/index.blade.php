4a573f8542349abb66188b148cde0007fddf4891
Fernando Lira <it.fernandolira@gmail.com>
2021-08-22 15:12:39 +0100

    Blade - @while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -8,6 +8,7 @@
         <h4>Não existem fornecedores registados!</h4>
     @endif
 
+    Com FOR<br>
     @for($i=0;$i<count($fornecedores);$i++)
         Nome: {{ $fornecedores[$i]['nome'] }}<br>
         Distrito:
@@ -24,6 +25,26 @@
         <br>
         <br>
     @endfor
+
+    Com WHILE<br>
+    @php $i=0 @endphp
+    @while(isset($fornecedores[$i]))
+        Nome: {{ $fornecedores[$i]['nome'] }}<br>
+        Distrito:
+        @switch($fornecedores[$i]['distrito'])
+            @case('13')
+                {{ $fornecedores[$i]['distrito'] }} - Porto
+                @break
+            @case('2')
+                {{ $fornecedores[$i]['distrito'] }} - Braga
+                @break
+            @default
+                Distrito desconhecido
+        @endswitch
+        <br>
+        <br>
+        @php $i++ @endphp
+    @endwhile
 
     @if(!@empty($fornecedores))
     {{-- Mostrar todo o conteúdo de um array--}}
