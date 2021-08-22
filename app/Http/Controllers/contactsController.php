d088e7cf06a46cb1ea41282cff98708fb028297b
Fernando Lira <it.fernandolira@gmail.com>
2021-08-22 20:38:36 +0100

    Envio de formulario de contacto

--- app/Http/Controllers/contactsController.php
+++ app/Http/Controllers/contactsController.php
@@ -8,6 +8,8 @@ class contactsController extends Controller
 {
     //função contacts
     public function contacts() {
+
+        var_dump($_GET);
         return view('site.contacts',['titulo'=>'First app - Contacto (variavel)']);
     }
 }
