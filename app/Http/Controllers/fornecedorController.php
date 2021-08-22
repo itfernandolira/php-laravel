5790ba5b3e5dc9edb0e9226e0ca110fe0348c769
Fernando Lira <it.fernandolira@gmail.com>
2021-08-22 14:32:07 +0100

    Blade - @switch / @case

--- app/Http/Controllers/fornecedorController.php
+++ app/Http/Controllers/fornecedorController.php
@@ -7,7 +7,16 @@ use Illuminate\Http\Request;
 class fornecedorController extends Controller
 {
     public function index() {
-        $fornecedores=['Fornecedor 1'];
+        $fornecedores=[
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'distrito' => '13'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'distrito' => '2'
+                ]
+        ];
 
         $msg = isset($fornecedores[1]) ? "Existe a posição 1." : "Não existe a posição 1";
         echo $msg;
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -8,8 +8,21 @@
         <h4>Não existem fornecedores registados!</h4>
     @endif
 
-    Primeiro: {{ $fornecedores[0] }}<br>
-    Segundo: {{ $fornecedores[1] ?? 'Não existe' }}<br>
+    Primeiro: <br>
+    Nome: {{ $fornecedores[0]['nome'] }}<br>
+    Distrito:
+    @switch($fornecedores[0]['distrito'])
+        @case('13')
+            {{ $fornecedores[0]['distrito'] }} - Porto<br>
+            @break
+        @case('2')
+            {{ $fornecedores[0]['distrito'] }} - Braga
+            @break
+    @endswitch
+
+    <br>
+    Segundo: <br>
+    {{ $fornecedores[1]['nome'] ?? 'Não existe' }}<br>
 
     @if(!@empty($fornecedores))
     {{-- Mostrar todo o conteúdo de um array--}}
