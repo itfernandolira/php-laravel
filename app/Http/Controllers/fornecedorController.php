d3b3b918f66a906520925de9dccc05e716545d44
Fernando Lira <it.fernandolira@gmail.com>
2021-08-22 14:07:33 +0100

    Operador condicional ternário - Nativo PHP

--- app/Http/Controllers/fornecedorController.php
+++ app/Http/Controllers/fornecedorController.php
@@ -9,6 +9,9 @@ class fornecedorController extends Controller
     public function index() {
         $fornecedores=['Fornecedor 1'];
 
+        $msg = isset($fornecedores[1]) ? "Existe a posição 1." : "Não existe a posição 1";
+        echo $msg;
+
         return view('app.fornecedor.index',compact('fornecedores'));
     }
 }
