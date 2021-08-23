e7dc6bafc082a384d2211d269670ac2a6716e24c
Fernando Lira <it.fernandolira@gmail.com>
2021-08-23 08:46:03 +0100

    Implementar a Migration

--- database/migrations/2021_08_23_072716_create_site_contactos_table.php
+++ database/migrations/2021_08_23_072716_create_site_contactos_table.php
@@ -16,6 +16,11 @@ class CreateSiteContactosTable extends Migration
         Schema::create('site_contactos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome',80);
+            $table->string('telefone',20);
+            $table->string('email',80);
+            $table->integer('motivo');
+            $table->text('mensagem'); 
         });
     }
 
