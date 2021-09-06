8603fc37bec269857ce2b1d4fba570ba32e629dd
Fernando Lira <it.fernandolira@gmail.com>
2021-09-06 17:39:06 +0100

    Save data - repopular o form

--- resources/views/site/layouts/_components/formcontacto.blade.php
+++ resources/views/site/layouts/_components/formcontacto.blade.php
@@ -1,11 +1,11 @@
 {{ $slot }}
 <form action="{{ route('site.contacts') }}" method="post">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="{{ $classe }}">
+    <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe }}">
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="{{ $classe }}">
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
     <select name="motivo" class="{{ $classe }}">
         <option value="0">Qual o motivo do contato?</option>
@@ -14,7 +14,8 @@
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{ $classe }}">@if(old('mensagem')!=''){{ old('mensagem') }}@else Preencha aqui a sua mensagem @endif    
+    </textarea>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
