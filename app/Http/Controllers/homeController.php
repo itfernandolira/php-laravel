b7bc1f3d2cfdd3405e23d999fc325571c6d54325
Fernando Lira <it.fernandolira@gmail.com>
2021-09-06 18:04:39 +0100

    Save data - melhorar o option value - corrigir na home

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -8,6 +8,13 @@ class homeController extends Controller
 {
     //função home
     public function home() {
-        return view('site.home');
+
+        $motivos=[
+            '1'=>'Dúvida',
+            '2'=>'Elogio',
+            '3'=>'Reclamação'
+        ];
+
+        return view('site.home',['motivos'=>$motivos]);
     }
 }
