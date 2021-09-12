5979fd06060b1dfca50d71e90d3685833d81ff67
Fernando Lira <it.fernandolira@gmail.com>
2021-09-12 16:54:16 +0100

    Middlewares - Implementar o middleware no Controller

--- app/Http/Controllers/aboutusController.php
+++ app/Http/Controllers/aboutusController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\logAccessMiddleware;
 use Illuminate\Http\Request;
 
 class aboutusController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(logAccessMiddleware::class);
+    }
     //função aboutus
     public function aboutus() {
         return view('site.aboutus');
