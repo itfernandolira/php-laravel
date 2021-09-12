d6bffb932a4c121b95d7ab3c9061c562e2df2c48
Fernando Lira <it.fernandolira@gmail.com>
2021-09-12 11:24:53 +0100

    Save data - validação de campos de email

--- app/Http/Controllers/contactsController.php
+++ app/Http/Controllers/contactsController.php
@@ -33,7 +33,7 @@ class contactsController extends Controller
         $request->validate([
             'nome'=>'required|min:3|max:40', //minimo de 3 caracteres e maximo de 40
             'telefone'=>'required',
-            'email'=>'required',
+            'email'=>'email',
             'motivo'=>'required',
             'mensagem'=>'required|max:2000'
         ]);
