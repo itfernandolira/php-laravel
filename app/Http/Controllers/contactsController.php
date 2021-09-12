0822b2f91fe643c31342740f453797d825531d29
Fernando Lira <it.fernandolira@gmail.com>
2021-09-12 12:40:57 +0100

    Save data - Personalização das mensagens de erro

--- app/Http/Controllers/contactsController.php
+++ app/Http/Controllers/contactsController.php
@@ -30,13 +30,27 @@ class contactsController extends Controller
     public function guardar(Request $request) {
 
         //executar a validação
-        $request->validate([
+        $request->validate(
+            [
             'nome'=>'required|min:3|max:40', //minimo de 3 caracteres e maximo de 40
             'telefone'=>'required',
             'email'=>'email',
             'motivos_id'=>'required',
             'mensagem'=>'required|max:2000'
-        ]);
+            ],
+            [
+                'nome.required'=>"Tem de indicar o nome",
+                'nome.min'=>"O nome tem de conter mais de 3 caracteres",
+                'nome.max'=>"O nome tem de conter menos de 40 caracteres",
+
+                //metodo generalista
+                'required'=>"Tem de indicar o :attribute",
+
+                'mensagem.max'=>"A mensagem tem de ter no máximo 2000 caracteres",
+                'email.email'=>"O email indicado não é válido"
+
+            ]
+    );
         
         //código mais limpo
         siteContacto::create($request->all());
