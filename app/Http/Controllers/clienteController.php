297725de14d05e8c61a8f99d945cbfd606ec4e07
Fernando Lira <it.fernandolira@gmail.com>
2021-09-23 13:48:22 +0100

    Listar todos os clientes com paginação

--- app/Http/Controllers/clienteController.php
+++ app/Http/Controllers/clienteController.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\cliente;
+
 
 use Illuminate\Http\Request;
 
 class clienteController extends Controller
 {
     public function index() {
-        return view('app.cliente.index');
+
+        $clientes=cliente::paginate(20);
+
+        return view('app.cliente.index',['clientes'=>$clientes]);
     }
 }
--- resources/views/app/cliente/index.blade.php
+++ resources/views/app/cliente/index.blade.php
@@ -1,3 +1,21 @@
 <h1>Clientes</h1>
 <hr>
+<table>
+    <tr>
+        <th>Nome</th>
+        <th>País</th>
+        <th>E-mail</th>
+    </tr>
+@foreach ($clientes as $cliente)
+    <tr>
+        <td>{{ $cliente->nome }}</td>
+        <td>{{ $cliente->pais }}</td>
+        <td>{{ $cliente->email }}</td>
+        <td>Editar</td>
+        <td>Apagar</td>
+    </tr>
+@endforeach
+</table>
+{{ $clientes->links() }}
+<hr>
 <p><a href="{{ route('app.sair') }}">LOGOUT</a></p>
